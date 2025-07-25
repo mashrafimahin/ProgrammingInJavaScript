dc8113303bebbe418bf8ec588b2ac9d6ccd526d6
Mashrafi Mahin <mashrafimahin120@gmail.com>
2025-07-26 00:54:01 +0600

    Two Sum Problem Solving

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-// Problem: 
-// Guess I have a store and I give a offer. If someone buy products over 3000tk will get a 5% discount and if the price is over 6000tk will get 15% discount. Now write down code for this discount value and return the Full Price after discount. 
-// Thinking (max 5 minutes)
-// Answer:
-function myFunction(value){
-    // let a simple string that can help to return a beautiful output
-    let back = "You'll get return: ";
-    // let have a discount value
-    let discount = 0;
-    // let have a final calculation
-    let result = 0;
-    // if shopping price > 6000 = 15% discount
-    if (value > 6000) {
-        discount = value / 100 * 15;
-        result = value - discount;
+
+
+// Two Sum Problem Solving 
+
+const twoSum = function(numbers , target) {
+    // empty array 
+    let myArr = [];
+    // run loop
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) {
+                myArr.push(i, j);
+            }
+        }
     }
-    // if shopping price > 3000 = 5% discount
-    else if (value > 3000) {
-        discount = value / 100 * 5;
-        result = value - discount;
-    }
-    // if shopping price < 3000 = no discount
-    else {
-        result = value;
-    }
-    // let a final output message for customer
-    let reply = back + result;
-    // return message
-    return reply; 
+    // return value
+    return myArr;
+
 }
-// let guess a price for your product and have a discount
-console.log(myFunction(4500));
-// Final Result: 4275.
+
+// number & target
+const numbers = [2,7,11,15];
+const target = 26;
+// print the function 
+const print = twoSum(numbers, target);
+// console
+console.log(print);
+
+
+
+
+
