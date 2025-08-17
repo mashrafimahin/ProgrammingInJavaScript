9217e1ab907db51967b21ae9dea7f59e462c657a
Mashrafi Mahin <mashrafimahin120@gmail.com>
2025-08-17 22:43:05 +0600

    Array Wrapper LeetCode

--- arrayWrapper.js
+++ arrayWrapper.js
@@ -1,11 +1,50 @@
 /*
- ! Problem: Check for palindrome
+ ! Problem: Arraw Wrapper Problem
 
  ? Proccess:
     *  take empty variable
-    *  add checkpoint
-    *  reverse the current value
-    *  match both values and return
+    *  add checkpoint for each data
+    *  create dynamic data inside object
+    *  add items to the data inside object and return
  */
 
+// data
+const data = [
+   {
+      name: 'Alice',
+      age : 21
+   },
+   {
+      name: 'Bob',
+      age : 17
+   },
+   {
+      name: 'Charlie',
+      age : 21
+   }
+]
+
+
 // function
+function myFunc(value) {
+
+   // empty object
+   let myObj = {}
+
+   // check for each data object
+   value.forEach(item => {
+
+      // check first condition
+      if (!myObj[item.age]) {
+         myObj[item.age] = []
+      }
+      
+      myObj[item.age].push(item.name)
+
+   });
+
+   // return the main object
+   return myObj
+}
+
+myFunc(data)
