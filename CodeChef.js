c98809507d83ffdacda518183eb434b83231545c
Mahin <mashrafi.devs@gmail.com>
2026-01-25 01:06:41 +0600

    Javascript

--- CodeChef.js
+++
@@ -1,6 +0,0 @@
-const hour = 5;
-const min = new Date().getMinutes();
-const sec = new Date().getSeconds();
-
-console.log(hour * 60 - min);
-console.log(hour * 3600 - sec);
--- LengthOfWord.js
+++
@@ -1,7 +0,0 @@
-const LastWord = (s) => {
-  const arr = s.split(" ");
-  const filtered = arr.filter((a) => a);
-  return filtered[filtered.length - 1].length;
-};
-
-LastWord(" Hello World ");
