bcbb70f3c6c31cb50acbb7853c51b3058e98023d
Mahin <mashrafi.devs@gmail.com>
2026-01-25 02:18:39 +0600

    Filter Elements from Array/LeetCode(2634)

---
+++ Easy/FilterArray.js
@@ -0,0 +1,37 @@
+//! Problem: Filter Elements from Array/LeetCode(2634)
+
+//? Description:
+//* Given an integer array arr and a filtering function fn, return a filtered array filteredArr.
+//* The fn function takes one or two arguments:
+//* arr[i] - number from the arr
+//* i - index of arr[i]
+//* filteredArr should only contain the elements from the arr for which the expression fn(arr[i], i) evaluates to a truthy value. A truthy value is a value where Boolean(value) returns true.
+//* Please solve it without the built-in Array.filter method.
+
+//? Example:
+//* Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+//* Output: [20,30]
+//* Explanation:
+//* const newArray = filter(arr, fn); // [20, 30]
+//* The function filters out values that are not greater than 10
+
+//! Solution:
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i], i)) newArr.push(arr[i]);
+  }
+
+  return newArr;
+};
+
+//! Testcase:
+const arr = [0, 10, 20, 30];
+const fn = (x) => x > 10;
+filter(arr, fn);
