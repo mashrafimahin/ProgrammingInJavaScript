adf90218b6dd77d49a73d2993335cd4f15ee33f9
Mahin <mashrafi.devs@gmail.com>
2026-02-10 20:18:53 +0600

    Snail Traversal

---
+++ Medium/SnailTraversal.js
@@ -0,0 +1,54 @@
+//! Problem: Snail Traversal/LeetCode(2624)
+
+//? Description:
+//* Write code that enhances all arrays such that you can call the snail(rowsCount, colsCount) method that transforms the 1D array into a 2D array organised in the pattern known as snail traversal order. Invalid input values should output an empty array. If rowsCount * colsCount !== nums.length, the input is considered invalid.
+//*
+//* Snail traversal order starts at the top left cell with the first value of the current array. It then moves through the entire first column from top to bottom, followed by moving to the next column on the right and traversing it from bottom to top. This pattern continues, alternating the direction of traversal with each column, until the entire current array is covered. For example, when given the input array [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15] with rowsCount = 5 and colsCount = 4, the desired output matrix is shown below. Note that iterating the matrix following the arrows corresponds to the order of numbers in the original array.
+
+//? Example:
+//* Input:
+//* nums = [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15]
+//* rowsCount = 5
+//* colsCount = 4
+//* Output:
+//* [
+//*  [19,17,16,15],
+//*  [10,1,14,4],
+//*  [3,2,12,20],
+//*  [7,5,18,11],
+//*  [9,8,6,13]
+//* ]
+
+//! Solution:
+/**
+ * @param {number} rowsCount
+ * @param {number} colsCount
+ * @return {Array<Array<number>>}
+ */
+Array.prototype.snail = function (rowsCount, colsCount) {
+  // invalid input
+  if (rowsCount * colsCount !== this.length) return [];
+
+  // new array
+  let result = Array(rowsCount)
+    .fill()
+    .map(() => []);
+
+  // target index
+  for (let row = 0; row < colsCount; row++) {
+    for (let col = 0; col < rowsCount; col++) {
+      // push targeted item to main array
+      result[row & 1 ? rowsCount - col - 1 : col].push(
+        this[row * rowsCount + col],
+      );
+    }
+  }
+
+  return result;
+};
+
+//! Testcases:
+const arr = [
+  19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15,
+];
+arr.snail(5, 4);
